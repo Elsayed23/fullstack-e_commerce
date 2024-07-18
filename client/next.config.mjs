bba06e614c52f390c3d2a14ffd5912d658d7395a
Elsayed23 <elsayedkewan123@gmail.com>
2024-07-18 04:13:29 +0300

    update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         domains: ['res.cloudinary.com', 'elsayed23.github.io']
     },
     env: {
-        API_BASE_URL: 'https://full-stack-e-commerce-goiy.onrender.com/api/v1',
+        API_BASE_URL: 'https://fullstack-ecommerce-server.vercel.app/api/v1',
         DATABASE_URL: "mysql://root:elsayed@localhost:3306/test"
     }
 };
